0af294b7f8eeef6e200ba984212954fc97a2d2be
navrocks1 <guleriathakur43@gmail.com>
2020-04-27 16:48:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,35 @@
 
+$(document).ready(function(){
+      var serv = $('#get_SERVICE').html();
+      $('#sticky-01').html(serv); 
+    });
+    
+    
+    var $sidebar = $("#sticky-01"),
+        $window = $(window);
+    $window.scroll(function () {
+
+        var footerTop = $('#shopify-section-footer').position().top
+        var sidebarBottom = $window.scrollTop() + 400;
+        if ($window.scrollTop() > 35 && $window.scrollTop() < 87) {
+            $(".sideBarWrapper").addClass('sideBarWrapperSmall');
+            $(".sideBarWrapper").removeClass('sideBarWrapperBig');
+
+        } else if ($window.scrollTop() > 86) {
+            $(".sideBarWrapper").removeClass('sideBarWrapperSmall');
+
+            $(".sideBarWrapper").addClass('sideBarWrapperBig');
+
+        }
+        else {
+            $(".sideBarWrapper").removeClass('sideBarWrapperSmall');
+
+            $(".sideBarWrapper").removeClass('sideBarWrapperBig');
+        }
+        if (sidebarBottom > footerTop) {
+            $('.sideBarWrapper').css('opacity', '0');
+        } else {
+            $('.sideBarWrapper').css('opacity', '1');
+        }
+
+    });
